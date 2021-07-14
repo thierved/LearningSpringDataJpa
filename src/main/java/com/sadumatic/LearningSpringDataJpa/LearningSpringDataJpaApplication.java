84ad0718fe4d18a2c45714914e84a4208f566f91
thierno sadou diallo <thierved01@gmail.com>
2021-07-14 15:35:30 +0000

    Initial commit

---
+++ src/main/java/com/sadumatic/LearningSpringDataJpa/LearningSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.sadumatic.LearningSpringDataJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringDataJpaApplication.class, args);
+	}
+
+}
